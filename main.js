4679517a2fcca870dca5aa187198d4d0096b001a
Arun8008 <arun.sivanpandian.2000@gmail.com>
2023-03-17 16:53:21 +0530

    update

---
+++ main.js
@@ -0,0 +1,18 @@
+
+let day=prompt("Enter The Day");
+switch(day){
+   case "sunday":
+      case "saturday":
+      document.write("weekend day");
+      break;
+   case "monday":
+       case "tuesday":
+         case "wednesday":
+            case "thurday":
+               case "friday":
+      document.write("working day");
+      break;   
+   default:
+      document.write("Enter The Day")
+   
+}
